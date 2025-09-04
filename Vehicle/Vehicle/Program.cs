3fecb7d32aa99961544b80bfead5c7be465c1d01
Disha <disha09github@gmail.com>
2025-09-04 13:44:20 +0530

    Add files via upload

---
+++ Vehicle/Vehicle/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Vehicle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c = new Car();
+            c.Model = "Honda City";
+            c.RentPerDay = 1500;
+            c.Seats = 5;
+            c.FuelType = "Petrol";
+            c.Show();
+        }
+    }
+}
