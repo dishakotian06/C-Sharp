aa6f66ea7f49489c5da6564f6e4e0b0e445f89e5
Disha <disha09github@gmail.com>
2025-09-04 14:12:53 +0530

    Add files via upload

---
+++ InsufficientFundsException/InsufficientFundsException/InsufficientFundsException.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsufficientFundsException
+{
+    class InsufficientFundsException : Exception
+    {
+        public InsufficientFundsException(string message) : base(message) { }
+    }
+
+    class BankAccount
+    {
+        private double balance;
+
+        public BankAccount(double initialBalance)
+        {
+            balance = initialBalance;
+        }
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+            Console.WriteLine("Deposited: " + amount);
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > balance)
+                throw new InsufficientFundsException("Not enough balance!");
+
+            balance -= amount;
+            Console.WriteLine("Withdrawn: " + amount);
+        }
+        public void DisplayBalance()
+        {
+            Console.WriteLine("Balance: " + balance);
+        }
+
+    }
+}
---
+++ InsufficientFundsException/InsufficientFundsException/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace InsufficientFundsException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount(1000);
+
+            try
+            {
+                account.DisplayBalance();
+                Console.Write("Enter amount to withdraw: ");
+                double w = Convert.ToDouble(Console.ReadLine());
+                account.Withdraw(w);
+                account.DisplayBalance();
+            }
+            catch (InsufficientFundsException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+        }
+    }
+}
