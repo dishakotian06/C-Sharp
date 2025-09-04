1ef84c14be3330dc33bfa522bda8c44a72a9e678
Disha <disha09github@gmail.com>
2025-09-04 14:19:35 +0530

    Add files via upload

---
+++ BookNotAvailableException/BookNotAvailableException/BookNotAvailableException.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookNotAvailableException
+{
+    class BookNotAvailableException : Exception
+    {
+        public BookNotAvailableException(string message) : base(message) { }
+    }
+
+    class Library
+    {
+        private Dictionary<string, bool> books = new Dictionary<string, bool>();
+
+        public Library()
+        {
+            books["Java"] = true;
+            books["Python"] = true;
+            books["C++"] = false; 
+        }
+
+        public void BorrowBook(string book)
+        {
+            if (books.ContainsKey(book) && books[book])
+            {
+                books[book] = false;
+                Console.WriteLine(book + " borrowed successfully!");
+            }
+            else
+            {
+                throw new BookNotAvailableException(book + " is not available!");
+            }
+        }
+    }
+}
+
+
---
+++ BookNotAvailableException/BookNotAvailableException/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace BookNotAvailableException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Library lib = new Library();
+
+            try
+            {
+                Console.Write("Enter book name: ");
+                string book = Console.ReadLine();
+                lib.BorrowBook(book);
+            }
+            catch (BookNotAvailableException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+        }
+    }
+}
