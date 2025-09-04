478ee84a315cbd7e649707eda2ff0adf56e208e4
Disha <disha09github@gmail.com>
2025-09-04 14:36:15 +0530

    Add files via upload

---
+++ InvalidMarksException/InvalidMarksException/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace InvalidMarksException
+{
+     class Program
+     {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter marks: ");
+                int marks = Convert.ToInt32(Console.ReadLine());
+                GradingSystem.CheckMarks(marks);
+            }
+            catch (InvalidMarksException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+        }
+     }
+     
+}
